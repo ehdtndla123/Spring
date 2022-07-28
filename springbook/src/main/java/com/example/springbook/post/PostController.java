27ac90ae9608577aa4dcb68c6befd3455a88984e
장동수 <ehdtndla123@naver.com>
2022-07-29 01:23:58 +0900

    마크다운 형식 적용

--- springbook/src/main/java/com/example/springbook/post/PostController.java
+++ springbook/src/main/java/com/example/springbook/post/PostController.java
@@ -2,6 +2,7 @@ package com.example.springbook.post;
 
 import com.example.springbook.coment.CommentForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,9 +20,9 @@ public class PostController {
 
 
     @RequestMapping("/list")
-    public String list(Model model){
-        List<Post> postList=this.postService.getList();
-        model.addAttribute("postList",postList);
+    public String list(Model model,@RequestParam(value = "page",defaultValue = "0") int page){
+        Page<Post> paging=this.postService.getList(page);
+        model.addAttribute("paging",paging);
         return "post_list";
     }
     @RequestMapping(value ="/detail/{id}")
--- springbook/src/main/java/com/example/springbook/post/PostService.java
+++ springbook/src/main/java/com/example/springbook/post/PostService.java
@@ -1,9 +1,14 @@
 package com.example.springbook.post;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,8 +17,11 @@ import java.util.Optional;
 public class PostService {
     private final PostRepository postRepository;
 
-    public List<Post> getList(){
-        return this.postRepository.findAll();
+    public Page<Post> getList(int page){
+        List<Sort.Order> list=new ArrayList<>();
+        list.add(Sort.Order.desc("createDate"));
+        Pageable pageable= PageRequest.of(page,10,Sort.by(list));
+        return this.postRepository.findAll(pageable);
     }
 
     public Post getPost(Integer id){
